e1978c5ea3cffd08ba11a5dbcddc327186e5bd18
londlord1 <londlordalexei@yandex.ru>
2026-09-10 08:52:39 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="py-2 my-2">
+    <ul class="nav justify-content-between align-items-center">
+        <li class="nav-item"><a href="#" class="nav-link text-body-secondary">ул. г.Москва, ул. Ивовая, 48</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-body-secondary">Время работы: Пн-Пт, с 8:00-17:00</a></li>
+        <li class="nav-item"><a href="tel:88005553535" class="nav-link text-body-secondary">тел. 8 (800) 555 - 35 - 35</a></li>
+        <li class="nav-item"><a href="mailto:обращения@СветлыеСны.рф" class="nav-link text-body-secondary">Email: обращения@СветлыеСны.рф</a></li>
+    </ul>
+</footer>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.7.1.slim.min.js"></script>
+<script src="js/jquery.inputmask.min.js"></script>
+<script src="js/index.js"></script>
+</body>
+</html>
